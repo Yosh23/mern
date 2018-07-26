b2359c92c68936ef5456ca78175b3cf09a515a3c
Josh Riches <joshriches@gmail.com>
2018-07-26 16:05:48 -0600

    Integrating ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,21 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import gql from 'graphql-tag'
+import {graphql, compose} from 'react-apollo'
+
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import Form from './Form'
+
+const TodosQuery = gql `
+{
+  todos {
+    id
+    text
+    complete
+  }  
+}
+`
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const {data: {loading, todos}} = this.props
+      if (loading) {
+        return null
+      }
+
+      const handleToggle = todo => () => {
+          const { checked } = this.state;
+          const currentIndex = checked.indexOf(todo);
+          const newChecked = [...checked];
+
+          if (currentIndex === -1) {
+              newChecked.push(todo);
+          } else {
+              newChecked.splice(currentIndex, 1);
+          }
+
+          this.setState({
+              checked: newChecked,
+          });
+      };
+
+
+      return (
+          <section style={{display: 'flex'}}>
+              <section style={{margin: 'auto', width: 400}}>
+                  <Paper elevation={10}>
+                      <Form/>
+                      <List>
+                          {todos.map(todo => (
+                              <ListItem
+                                  key={todo.id}
+                                  role={undefined}
+                                  dense
+                                  button
+                                  onClick={handleToggle(todo)}
+                              >
+                                  <Checkbox
+                                      checked={todo.complete}
+                                      tabIndex={-1}
+                                      disableRipple
+                                  />
+                                  <ListItemText primary={todo.text} />
+                                  <ListItemSecondaryAction>
+                                      <IconButton>
+                                          <DeleteIcon />
+                                      </IconButton>
+                                  </ListItemSecondaryAction>
+                              </ListItem>
+                          ))}
+                      </List>
+                  </Paper>
+              </section>
+          </section>
+      );
   }
 }
 
-export default App;
+export default compose(
+    graphql(TodosQuery)
+)(App);
---
+++ ui/src/Form.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+import TextField from '@material-ui/core/TextField';
+
+
+export default class extends Component {
+    render() {
+        return (
+            <TextField
+                label="todo..."
+                margin="normal"
+            />
+        )
+    }
+}
